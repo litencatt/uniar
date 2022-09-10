71cece48cea88d2b2b2a943fe646d4a20f67b5cf
litencatt <ncl0709@gmail.com>
2022-09-10 11:07:25 +0900

    Move to list

---
+++ cmd/list.go
@@ -0,0 +1,61 @@
+/*
+Copyright © 2022 Kosuke Nakamura <ncl0709@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/litencatt/unisonair/repository"
+	"github.com/spf13/cobra"
+	"github.com/xo/dburl"
+)
+
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "A brief description of your command",
+	Long:  `A longer description`,
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx := context.Background()
+		dsn := os.Getenv("UNIAR_DSN")
+		db, err := dburl.Open(dsn)
+		if err != nil {
+			log.Print(err)
+		}
+
+		queries := repository.New(db)
+
+		// list all authors
+		groups, err := queries.GetGroup(ctx)
+		if err != nil {
+			log.Print(err)
+		}
+		fmt.Println(groups)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+}
--- go.mod
+++ go.mod
@@ -3,13 +3,13 @@ module github.com/litencatt/unisonair
 go 1.19
 
 require (
-	github.com/go-sql-driver/mysql v1.6.0
 	github.com/k0kubun/sqldef v0.13.8
 	github.com/spf13/cobra v1.5.0
 	github.com/xo/dburl v0.12.3
 )
 
 require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/k0kubun/pp/v3 v3.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
