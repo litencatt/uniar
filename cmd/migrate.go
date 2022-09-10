e99f31b824f65d02b73f3a8b14edea35ce50d6d2
litencatt <ncl0709@gmail.com>
2022-09-10 10:48:10 +0900

    Move migration func to CLI command

---
+++ cmd/migrate.go
@@ -0,0 +1,89 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/k0kubun/sqldef"
+	"github.com/k0kubun/sqldef/database"
+	"github.com/k0kubun/sqldef/database/mysql"
+	"github.com/k0kubun/sqldef/parser"
+	"github.com/k0kubun/sqldef/schema"
+	"github.com/litencatt/unisonair/sql"
+	"github.com/spf13/cobra"
+	"github.com/xo/dburl"
+)
+
+var (
+	options = sqldef.Options{}
+)
+
+// migrateCmd represents the migrate command
+var migrateCmd = &cobra.Command{
+	Use:   "migrate",
+	Short: "A brief description of your command",
+	Long:  `A longer description`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := execute(); err != nil {
+			log.Print(err)
+		}
+	},
+}
+
+func execute() error {
+	dsn := os.Getenv("UNIAR_DSN")
+	u, _ := dburl.Parse(dsn)
+	password, _ := u.User.Password()
+	port, _ := strconv.Atoi(u.Port())
+	config := database.Config{
+		DbName:   strings.TrimPrefix(u.Path, "/"),
+		User:     u.User.Username(),
+		Password: password,
+		Host:     u.Hostname(),
+		Port:     port,
+	}
+	if options.DesiredFile == "" {
+		f, err := os.CreateTemp("", "schema")
+		if err != nil {
+			return err
+		}
+		defer func() {
+			filename := f.Name()
+			if err := os.Remove(filename); err != nil {
+				fmt.Println("一時ファイルの削除中にエラーが発生しました")
+			}
+		}()
+		options.DesiredFile = f.Name()
+		if _, err := f.Write(sql.Schema); err != nil {
+			return err
+		}
+		if err := f.Close(); err != nil {
+			return err
+		}
+	}
+	options := &options
+	db, err := mysql.NewDatabase(config)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := db.Close(); err != nil {
+			fmt.Println("データベースの切断中にエラーが発生しました")
+		}
+	}()
+
+	sqlParser := database.NewParser(parser.ParserModeMysql)
+	sqldef.Run(schema.GeneratorModeMysql, db, sqlParser, options)
+
+	return nil
+}
+
+func init() {
+	rootCmd.AddCommand(migrateCmd)
+}
--- main.go
+++ main.go
@@ -2,20 +2,11 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
-	"os"
-	"strconv"
-	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/k0kubun/sqldef"
-	"github.com/k0kubun/sqldef/database"
-	"github.com/k0kubun/sqldef/database/mysql"
-	"github.com/k0kubun/sqldef/parser"
-	"github.com/k0kubun/sqldef/schema"
 	"github.com/litencatt/unisonair/repository"
-	"github.com/litencatt/unisonair/sql"
 	"github.com/xo/dburl"
 )
 
@@ -43,53 +34,6 @@ func run(dsn string) error {
 	return nil
 }
 
-func migrate(dsn string) error {
-	u, err := dburl.Parse(dsn)
-	password, _ := u.User.Password()
-	port, _ := strconv.Atoi(u.Port())
-	config := database.Config{
-		DbName:   strings.TrimPrefix(u.Path, "/"),
-		User:     u.User.Username(),
-		Password: password,
-		Host:     u.Hostname(),
-		Port:     port,
-	}
-	if options.DesiredFile == "" {
-		f, err := os.CreateTemp("", "schema")
-		if err != nil {
-			return err
-		}
-		defer func() {
-			filename := f.Name()
-			if err := os.Remove(filename); err != nil {
-				fmt.Errorf("一時ファイルの削除中にエラーが発生しました")
-			}
-		}()
-		options.DesiredFile = f.Name()
-		if _, err := f.Write(sql.Schema); err != nil {
-			return err
-		}
-		if err := f.Close(); err != nil {
-			return err
-		}
-	}
-	options := &options
-	db, err := mysql.NewDatabase(config)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if err := db.Close(); err != nil {
-			fmt.Errorf("データベースの切断中にエラーが発生しました")
-		}
-	}()
-
-	sqlParser := database.NewParser(parser.ParserModeMysql)
-	sqldef.Run(schema.GeneratorModeMysql, db, sqlParser, options)
-
-	return nil
-}
-
 func seed(dsn string) error {
 	ctx := context.Background()
 	db, err := dburl.Open(dsn)
@@ -111,10 +55,6 @@ func seed(dsn string) error {
 
 func main() {
 	dsn := "mysql://root@db:3306/unisonair?parseTime=true&loc=Asia%2FTokyo"
-
-	if err := migrate(dsn); err != nil {
-		log.Fatal(err)
-	}
 	if err := seed(dsn); err != nil {
 		log.Fatal(err)
 	}
