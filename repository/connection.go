f4f5ac4a15662c8f4bdce89441fbf842a70e3b13
litencatt <ncl0709@gmail.com>
2022-09-19 17:40:53 +0900

    Connect to sqlite3 db created in ~/.uniar

--- repository/connection.go
+++ repository/connection.go
@@ -3,12 +3,19 @@ package repository
 import (
 	"database/sql"
 	"fmt"
+	"os/user"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func NewConnection() (*sql.DB, error) {
-	db, err := sql.Open("sqlite3", "./uniar.db")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	uniarPath := user.HomeDir + "/.uniar"
+	dbPath := uniarPath + "/uniar.db"
+	db, err := sql.Open("sqlite3", dbPath)
 	if err != nil {
 		return nil, fmt.Errorf("データベース接続エラー")
 	}
